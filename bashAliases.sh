6bb014972ccd9d04ee07ed2be81a3a535773648d
Max Zimmerman <ZimmermanWilliamM@JohnDeere.com>
2017-10-12 08:39:19 -0500

    Organized functions and set aliases for easier calling and documentation

--- bashAliases.sh
+++ bashAliases.sh
@@ -18,15 +18,8 @@ alias imout="shutdown -s -t 0"
 alias restart="shutdown -r -t 0"
 alias lock="rundll32.exe user32.dll,LockWorkStation"
 alias clr="clear"
-
-function grp() {
-    grep -r -n -Z --color "$1" .
-}
-
-function mcd () {
-    mkdir -p $1
-    cd $1
-}
+alias grp="'customGrep'"
+alias mcd="'mkdirAndCd'"
 
 #Emacs Aliases
 alias enw="emacs -q -nw -Q"
@@ -39,13 +32,23 @@ alias vsc="/c/bench/tools/cmder/vendor/vsCode/Code.exe $1 -new_console"
 alias edit="$PREFERRED_EDITOR $1 -new_console"
 alias vs="'visualStudio'"
 
-function visualStudio(){
-    c:/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 14.0/Common7/IDE/devenv.exe $1 &
-}
-
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.rb rebase
 git config --global alias.st status
 #git config --global alias.logg "log --graph --decorate --author-date-order --date=relative --format=format:'%C(bold cyan)%h%C(reset) %C(white)%s%C(bold yellow)%d%C(reset)%n''        %C(cyan)%aD %C(bold green)(%ar)%C(reset)%C(green) - %an' --all"
+
+#Functions
+function visualStudio(){
+    c:/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 14.0/Common7/IDE/devenv.exe $1 &
+}
+
+function customGrep() {
+    grep -r -n -Z --color "$1" .
+}
+
+function mkdirAndCd() {
+    mkdir -p $1
+    cd $1
+}
