d74ddc601090e0bc1273f05f20a5e535f90f9e66
WMaxZimmerman <william.m.zimmerman@gmail.com>
2018-11-19 12:21:48 -0600

    Added aliases for csrun

--- dotnetAliases.sh
+++ dotnetAliases.sh
@@ -5,6 +5,14 @@ function coresln?() {
     echo "$indentString solutionName: The name of the application that you are making."
 }
 
+alias csrun="'runDotnetFrameworkProject'"
+function csrun?() {
+    echo "Builds and runs the given dotnet project."
+    echo "Parameters (projectName)"
+    echo "$indentString projectName: The name of the project that you want to run."
+    echo "NOTE: You should be in the solution root when running this command."
+}
+
 function runDotnetFrameworkProject {
     project=$1
     msbuild.exe "$project/$project.csproj"
@@ -15,7 +23,6 @@ function runDotnetFrameworkProject {
     
     $project/bin/debug/$project.exe
 }
-
 
 function createDotnetNtierProject {
     local projectName=$1
